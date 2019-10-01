bc0b1a5284c4b65efa4ff1a952469f85059e3b70
Stewart <jbs136@txstate.edu>
2019-09-30 19:25:16 -0500

    Defined a function for the welcome message.

--- javascript/external.js
+++ javascript/external.js
@@ -13,10 +13,10 @@ document.write(out);
 
 // A prompt box is used to prompt users to input a value before entering a page.
 
-
+/*
 user_name = window.prompt("Please enter your name", "Type your name here");
 document.write(user_name);
-
+*/
 
 /*
 x = 22;
@@ -42,3 +42,18 @@ function condition()
   }
 }
 */
+
+function welcome()
+{
+  let a = "Please enter your name.";
+  let b = "Type your name here.";
+
+  // A prompt box is used to prompt users to input a value before entering a page.
+  user_name = window.prompt(a, b);
+
+  message = "<h1>Hello, welcome to my webpage, " + user_name + "!</h1>"
+
+  return message
+}
+
+document.write(welcome());
